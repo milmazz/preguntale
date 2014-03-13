2b52c12ee5d09cc94d46809fe90869c3e414d7a5
Milton Mazzarri <milmazz@gmail.com>
2014-03-12 20:14:07 -0600

    Added more Sphinx dependencies.

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,7 @@ sys.path.insert(0, os.path.abspath('..'))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinxcontrib.httpdomain", "sphinx.ext.todo"]
+extensions = ["sphinxcontrib.httpdomain"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
